f39a4bb317546f0d5eb26dbdd36e4ca17ac8d4f0
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-11-04 03:51:31 -0300

    13:45 of video https://www.youtube.com/watch?v=tEMrD9t85v4

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Shop from "./pages/shop/Shop"
+import Cart from "./pages/cart/Cart"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <div className="App">
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />          
+          <Route path='/cart' element={<Cart />} />                  
+        </Routes>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div className="Cart">
+      CART
+    </div>
+  )
+}
+
+export default Cart
