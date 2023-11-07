c0ccf342e9f82c23b4f44d3fb8fcbb6f4f63b5a9
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-11-07 14:57:14 -0300

    Updating itemCount on cart, 50:44 of the video https://www.youtube.com/watch?v=tEMrD9t85v4

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react'; 
+import React, { useContext } from 'react'
+import { ShopContext } from "../../context/ShopContext"
 
 function Product(props) {
 	const { id, productName, price, productImage } = props.data
+	const { cartItems, addToCart, removeFromCart } = useContext(ShopContext)
 	
 	return (
 		<div className="cartItem">
@@ -9,6 +11,13 @@ function Product(props) {
 			<div className="description"	>
 				<p><b>{productName}</b></p>
 				<p>$ {price}</p>
+
+				<div className="countHandler">
+					<button onClick={ () => removeFromCart(id) }> - </button>
+					<input value={cartItems[id]} onChange={ (e) => } />
+					<button onClick={ ()=> addToCart(id) }> + </button>
+				</div>
+
 			</div>
 		</div>
 	)
